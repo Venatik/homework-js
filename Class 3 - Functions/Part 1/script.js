984ec8a16e8ae86f8bb95ce813617f24bde4b49c
Venatik <stefan.trajkovski008@gmail.com>
2023-12-07 23:32:50 +0100

    new file:   Class 3 - Functions/Part 1/index.html 	new file:   Class 3 - Functions/Part 1/script.js 	new file:   Class 3 - Functions/Part 2/index.html 	new file:   Class 3 - Functions/Part 2/script.js 	deleted:    README.md

---
+++ Class 3 - Functions/Part 1/script.js
@@ -0,0 +1,11 @@
+function typeOf(parameter) {
+ console.log(typeof parameter);
+}
+
+typeOf(null);
+typeOf(true);
+typeOf(10);
+typeOf("Hello G7!");
+// Ne sum siguren koe se bara, undefined ili random variabla (vo ovoj slucaj someVariable) koja ne e deklarirana.
+typeOf(undefined);
+typeOf(someVariable);
