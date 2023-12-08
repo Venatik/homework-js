77caab974f2b188e94c2ed92ae247f3577f8eb3b
Stefan Trajkovski <stefan.trajkovski008@gmail.com>
2023-12-08 01:51:10 +0100

    modified:   Class 3 - Functions/Part 2/script.js

--- Class 3 - Functions/Part 2/script.js
+++ Class 3 - Functions/Part 2/script.js
@@ -1,7 +1,13 @@
-function dogAge(age) {
-    let dogYears = 7 * age;
-    console.log(`Your dog is ${dogYears} years old in dog years!`);
+function birthYear(year) {
+    input = prompt("What is your or your dog's birth year?");
+    if (input >= 2010) {
+        let result = (new Date().getFullYear() - input) * 7;
+        console.log(`Your dog is ${result} years old in human years!`);
+    }
+    else if (input < 2010) {
+        let result2 = (new Date().getFullYear() - input) / 7;
+        console.log(`You are ${result2} years old in dog years!`);
+}
 }
 
-
-dogAge(2);
+birthYear(1989);
