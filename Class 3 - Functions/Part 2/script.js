670763d5f98321f13e1aa4ddbe5f68ec07f4687d
Stefan Trajkovski <stefan.trajkovski008@gmail.com>
2023-12-09 11:20:55 +0100

    modified:   Class 3 - Functions/Part 2/script.js 	modified:   Class 3 - Functions/Part 3/script.js

--- Class 3 - Functions/Part 2/script.js
+++ Class 3 - Functions/Part 2/script.js
@@ -7,6 +7,9 @@ function birthYear(year, species) {
         let result2 = (new Date().getFullYear() - year) / 7;
         console.log(`You are ${result2} years old in dog years!`);
 }
+    else {
+        console.log("Please enter a valid species! Valid species are dog and human.");
+    }
 }
 
 birthYear(2017, "dog");
--- Class 3 - Functions/Part 3/script.js
+++ Class 3 - Functions/Part 3/script.js
@@ -1,1 +1,13 @@
-let input = prompt("How much would you like to withdraw?");
+function atmWithdraw() {
+    let balance = 1000; // Ako ja koristime funkcijata povise pati, ovaa varijabla bi bila nadvor od funkcijata
+    let amount = Number(prompt("How much would you like to withdraw?"));
+    if (amount > balance) {
+        console.log("Insufficient funds!");
+    }
+    else if (amount <= balance) {
+        balance -= amount;
+        console.log(`You have withdrawn ${amount} and your new balance is ${balance}.`);
+    }
+}
+
+atmWithdraw();
