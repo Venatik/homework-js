9c48942f367c71a745a17a2433929e45caeaa7ae
Stefan Trajkovski <stefan.trajkovski008@gmail.com>
2023-12-09 11:40:31 +0100

    modified:   Class 3 - Functions/Part 3/script.js

--- Class 3 - Functions/Part 3/script.js
+++ Class 3 - Functions/Part 3/script.js
@@ -1,8 +1,8 @@
 function atmWithdraw() {
     let balance = 1000; // Ako ja koristime funkcijata povise pati, ovaa varijabla bi bila nadvor od funkcijata
-    let amount = Number(prompt("How much would you like to withdraw?"));
+    let amount = Number(prompt(`Your current balance is ${balance}. How much would you like to withdraw?`));
     if (amount > balance) {
-        console.log("Insufficient funds!");
+        console.log(`Your current balance is: ${balance}. You cannot withdraw ${amount}.`);
     }
     else if (amount <= balance) {
         balance -= amount;
