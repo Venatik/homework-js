cec7afb661b4a3b38346f4d179ff5b1f45b73f47
Venatik <stefan.trajkovski008@gmail.com>
2023-12-10 15:43:52 +0100

    new file:   Class 3 - Functions/Part 3/v2/index.html 	new file:   Class 3 - Functions/Part 3/v2/media/ATM-Machine.jpg 	new file:   Class 3 - Functions/Part 3/v2/script.js 	new file:   Class 3 - Functions/Part 3/v2/style.css

---
+++ Class 3 - Functions/Part 3/v2/script.js
@@ -0,0 +1,17 @@
+let btn = document.getElementById('button');
+let withdrawAmount = document.getElementById('amount');
+let result_balance = document.getElementById('result_balance');
+
+btn.addEventListener('click', atmWithdraw);
+
+function atmWithdraw() {
+    let balance = 1000;
+    let amount = Number(withdrawAmount.value);
+    if (amount > balance) {
+        result_balance.innerHTML = (`Your current balance is: ${balance}. You cannot withdraw ${amount}.`);
+    }
+    else if (amount <= balance) {
+        balance -= amount;
+        result_balance.innerHTML = (`You have withdrawn ${amount} and your new balance is ${balance}.`);
+    }
+}
