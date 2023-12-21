b98947c0403d5e0171eab732ca66d7df16ca677a
Venatik <stefan.trajkovski008@gmail.com>
2023-12-21 10:49:35 +0100

    modified:   Class 5 - DOM/Bonus/script.js

--- Class 5 - DOM/Bonus/script.js
+++ Class 5 - DOM/Bonus/script.js
@@ -1,29 +1,27 @@
 let recipeName = prompt("What is the name of the recipe?");
-let ingredientNumber = prompt("How many ingredients are there?"); // Ne e relevantno
 let ingredientList = document.getElementById("ingredients");
 
-function recipeFinal(name, content) {
+function recipeFinal(name, ingredients) {
     let recipeDiv = document.getElementById("recipe");
-    let h1 = document.createElement('h1');
+    let h1 = document.createElement("h1");
     h1.textContent = name;
     recipeDiv.appendChild(h1);
-    let h3 = document.createElement('h3');
+    let h3 = document.createElement("h3");
     h3.textContent = "Ingredients:";
     recipeDiv.appendChild(h3);
-    // let ingredients = content.split(",");
     for(let i=0; i < ingredients.length; i++) {
-        let li = document.createElement('li');
+        let li = document.createElement("li");
         li.textContent = ingredients[i].trim();
         ingredientList.appendChild(li);
     }
     recipeDiv.appendChild(ingredientList);
 }
 
-for(let i=0; i < content; i++) {
-    let ingredientContent = prompt(`Name every needed ingredient ${i + 1} `);
-    let li = document.createElement('li');
-    li.textContent = ingredientContent.trim();
-    ingredientList.appendChild(li);
+let numberOfIngredients = prompt("How many ingredients does the recipe need?");
+let ingredients = [];
+for(let i=0; i < numberOfIngredients; i++) {
+    let ingredientName = prompt(`Name ingredient ${i + 1}: `);
+    ingredients.push(ingredientName);
 }
 
-recipeFinal(recipeName, ingredientContent);
+recipeFinal(recipeName, ingredients);
